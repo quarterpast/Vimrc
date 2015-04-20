20f8f84214d7388516eb1b2e729afc96621a8ac5
Matt Brennan <mattyb1000@gmail.com>
2015-04-20 19:28:28 +0100

    checkout master before installing

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ ln -sf "$(pwd)/nvimrc" ~/.nvimrc
 ln -sf "$(pwd)/nvimrc" ~/.vimrc
 ln -sf ~/.nvim ~/.vim
 
+pushd bundle/neobundle.vim
+	git checkout master
+popd
+
 bundle/neobundle.vim/bin/neoinstall
