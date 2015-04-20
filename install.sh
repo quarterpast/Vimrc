876a5d667a442e3109d01ad49d900a292c7b98de
Matt Brennan <mattyb1000@gmail.com>
2015-04-20 11:14:11 +0100

    link, install, don't ignore modules

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -x -e
 
-ln -sf $"(pwd)/nvimrc" ~/.nvimrc
+ln -sf "$(pwd)/nvimrc" ~/.nvimrc
+ln -sf "$(pwd)/nvimrc" ~/.vimrc
+ln -sf ~/.nvim ~/.vim
+
+bundle/neobundle.vim/bin/neoinstall
